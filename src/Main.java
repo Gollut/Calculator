d78918e963b41f6bf3e188ef729b6acfd95c9571
Artem Dubovitskiy <a.dubovitskiy@allsafe.group>
2019-10-10 09:26:59 +0300

    Strange condition fix

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,7 @@ public class Main{
         } else {
             Calculator calculator = new Calculator();
             Double result = calculator.calculate(st);
-            if (result.equals(-1.111111)) {
-                System.out.println("Wrong expression");
-            } else {
-                System.out.println(result);
-            }
+            System.out.println(result);
         }
     }
 }
